477b0e1d8129b718c0a3a5d34d02a9954366d201
Ira <0376766@gmail.com>
2022-05-09 15:40:39 +0200

    added cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+import classes from "./Cart.module.css";
+
+export const Cart = () => {
+  const cartItems = <ul className={classes["cart-items"]}>{[]}</ul>;
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
